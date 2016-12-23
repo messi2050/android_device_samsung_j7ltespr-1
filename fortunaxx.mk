0bfab4e48b530991d87b0671f3f873eb0b7d9fd8
ZIM <zim555@mail.ru>
2016-12-23 21:33:28 +0200

    Disable adb by default

--- fortunaxx.mk
+++ fortunaxx.mk
@@ -229,7 +229,7 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.secure=0 \
     ro.adb.secure=0 \
     ro.debuggable=1 \
-    persist.service.adb.enable=1
+    persist.service.adb.enable=0
 
 # KSM
 PRODUCT_PROPERTY_OVERRIDES += \
