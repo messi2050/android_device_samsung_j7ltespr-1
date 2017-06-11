abe023bf83b2836cb02314f69572c2907aa1fbef
minz1 <emerytang@gmail.com>
2017-06-11 18:12:57 -0400

    j7ltespr: inherit proper dalvik config

--- device.mk
+++ device.mk
@@ -266,7 +266,7 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 $(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
 
 # Dalvik heap config
-include frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk
+include frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk
 
 # Texture config.
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
